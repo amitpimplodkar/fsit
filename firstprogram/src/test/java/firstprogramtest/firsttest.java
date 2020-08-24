d037528849980677c3b182446e46bee356953755
amitp <amitp@DESKTOP-AGLQFM8>
2020-08-24 14:29:48 +0530

    first commit

---
+++ firstprogram/src/test/java/firstprogramtest/firsttest.java
@@ -0,0 +1,10 @@
+package firstprogramtest;
+
+public class firsttest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("first maven project test");
+	}
+
+}
