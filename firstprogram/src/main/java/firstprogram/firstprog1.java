4357a8fd36b50a847969d5fe5767f1fbc5ca0332
amitp <amitp@DESKTOP-AGLQFM8>
2020-08-24 14:41:56 +0530

    second commit

--- firstprogram/src/main/java/firstprogram/firstprog1.java
+++ firstprogram/src/main/java/firstprogram/firstprog1.java
@@ -5,6 +5,7 @@ public class firstprog1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("first maven project");
+      System.out.println("first maven project secound line");
 	}
 
 }
--- firstprogram/src/test/java/firstprogramtest/firsttest.java
+++ firstprogram/src/test/java/firstprogramtest/firsttest.java
@@ -5,6 +5,8 @@ public class firsttest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 System.out.println("first maven project test");
+	      System.out.println("first maven project secound line test");
+
 	}
 
 }
